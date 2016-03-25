19cc1fb5d5cdf1e172bf83b036940a2142c94458
星野恵瑠 <shonetesmith@gmail.com>
2016-03-26 01:41:57 +0900

    Add initial Model Controller View classes

---
+++ SwiftMVCWithDataSourceModel/SwiftMVCWithDataSourceModel/Source/Models/MyModel.swift
@@ -0,0 +1,13 @@
+//
+//  MyModel.swift
+//  SwiftMVCWithDataSourceModel
+//
+//  Created by 史翔新 on 2016/03/26.
+//  Copyright © 2016年 史翔新. All rights reserved.
+//
+
+import UIKit
+
+class MyModel: NSObject {
+
+}
---
+++ SwiftMVCWithDataSourceModel/SwiftMVCWithDataSourceModel/Source/Views/MyView.swift
@@ -0,0 +1,21 @@
+//
+//  MyView.swift
+//  SwiftMVCWithDataSourceModel
+//
+//  Created by 史翔新 on 2016/03/26.
+//  Copyright © 2016年 史翔新. All rights reserved.
+//
+
+import UIKit
+
+class MyView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
